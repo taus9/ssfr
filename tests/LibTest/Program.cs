d990967af02258f53efdbe5b9915c059a6ea9777
Christopher Martinez <crmartinez239@protonmail.com>
2025-11-14 15:57:51 -0500

    Fix save file path

--- tests/LibTest/Program.cs
+++ tests/LibTest/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string path = "/home/baker/SkySaves/SkySave Test/saves/quicksave.ess";
+
+ssfr.SaveFile saveFile = new ssfr.Skyrim().LoadFile(path);
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine(saveFile.HeaderSize.ToString());
+
