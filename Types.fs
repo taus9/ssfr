d1219c5085a248df425ac130a1156712513ee04b
Christopher Martinez <crmartinez239@protonmail.com>
2025-11-14 13:14:12 -0500

    Add Header type def

--- Types.fs
+++ Types.fs
@@ -1,3 +1,21 @@
+type Header = {
+    Version: uint32
+    SaveNumber: uint32
+    PlayerName: string
+    PlayerLevel: uint32
+    PlayerLocation: string
+    GameDate: string
+    PlayerRaceEditorID: string
+    PlayerSex: uint16
+    PlayerCurrentExp: float32
+    PlayerLevelUpExp: float32
+    //Filetime: FILETIME
+    ShotWidth: uint32
+    ShotHeight: uint32
+    CompressionType: uint16
+}
+
+
 type SaveFile = {
     Magic:char[13]
     HeaderSize: uint32
