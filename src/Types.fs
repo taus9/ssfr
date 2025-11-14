8a060d5e46d932b5458e72f55ad62b1c418b2ee4
Christopher Martinez <crmartinez239@protonmail.com>
2025-11-14 15:57:18 -0500

    Removed unused types

--- src/Types.fs
+++ src/Types.fs
@@ -19,27 +19,6 @@ type Header = {
 
 
 type SaveFile = {
-    //Magic: string
     HeaderSize: uint32
-    //Header: Header
-    (*
-    ScreenshotData: LEScreenshot | SEScreenshot
-    UncompressedLen: uint32
-    CompressedLen: uint32
-    FormVersion: uint8
-    PluginInfoSize: uint32
-    PluginInfo: PluginInfo
-    LightPluginInfo: LightPluginInfo
-    FileLocationTable: FileLocationTable
-    GlobalDataTable1: GlobalData[]
-    GlobalDataTable2: GlobalData[]
-    ChangeForms: ChangeForm
-    GlobalDataTable3: GlobalData[]
-    FormIDArrayCount: uint32
-    FormIDArray: FormId[]
-    VisitedWorldspaceArrayCount: uint32
-    VistedWorldspaceArray: FormID[]
-    Unknown3TableSize: uint32
-    Uknown3Table: Unknown3Table
-    *)
+    Header: Header
 }
