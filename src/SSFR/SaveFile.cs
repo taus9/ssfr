be12e521d359b7708c069fae1828c77c23fa242f
Christopher Martinez <crmartinez239@protonmail.com>
2025-11-15 17:53:56 -0500

    Add magic check and HeaderSize

--- src/SSFR/SaveFile.cs
+++ src/SSFR/SaveFile.cs
@@ -1,6 +1,26 @@
-﻿namespace SSFR;
+﻿using System.Text;
+
+namespace SSFR;
 
 public class SaveFile
 {
+    private readonly byte[] _magic = Encoding.ASCII.GetBytes("TESV_SAVEGAME");
+
+    public uint HeaderSize { get; private set; }
+
+    public void LoadFile(string filePath)
+    {
+        using var stream = File.OpenRead(filePath);
+        using var reader = new BinaryReader(stream);
+        
+        byte[] magic = reader.ReadBytes(_magic.Length);
+        if (magic != _magic)
+        {
+            throw new Exception("Invalid file format");
+        }
+
+        HeaderSize = reader.ReadUInt32();
+
+    }
 
 }
